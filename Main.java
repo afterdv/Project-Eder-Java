43f0b9fd4dea09cb5bef360b3dace792a7c7a285
afterdv <noreplyesckkzteam@gmail.com>
2026-04-22 14:25:01 -0300

    Initialize Main.java with basic structure

---
+++ Main.java
@@ -0,0 +1,11 @@
+package io.github.afterdv.pej;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String []args) {
+    Scanner atp = new Scanner(System.in);
+    // Project Blank, Write Everyone Code
+  }
+}
